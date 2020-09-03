905a8a29863c3b4eff4356f003d19a249cbc61bc
SagittariusA <nilsole2009@live.no>
2020-09-03 18:15:44 +0200

    Now generalized sim2TOD to take runslist and parameterfile

--- src/sim/mapmaker_lite.py
+++ src/sim/mapmaker_lite.py
@@ -10,16 +10,18 @@ from tqdm import trange
 import sys
 
 t0 = time.time()
-"""
-data_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/level2/Ka/sim/co6/"
 
-histo = np.zeros((120, 120))
+# L2
+data_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/level2/Ka/sim/wo_mask/co6/"
 
 nside       = 120
+histo = np.zeros((nside, nside))
+
 dpix        = 2.0 / 60.0
 fieldcent   = [226, 55]
 nfeeds      = 18
-nbin        = 120 * 120
+nbin        = nside * nside
+
 looplen = 0
 for i in trange(2, 11, 1):
     filename   = data_path + f"co6_0015330{i:02d}.h5"
@@ -34,11 +36,11 @@ for i in trange(2, 11, 1):
     for j in range(nfeeds):  
         looplen += 1
         px_idx[j, :] = WCS.ang2pix([nside, nside], [-dpix, dpix], fieldcent, dec[j, :], ra[j, :])
-        map, edges   = np.histogram(px_idx[j, :], bins = 120 * 120, range = (0, 120 * 120), weights = tod[j, 2, 9, :]) 
-        nhit, edges   = np.histogram(px_idx[j, :], bins = 120 * 120, range = (0, 120 * 120))
+        map, edges   = np.histogram(px_idx[j, :], bins = nside * nside, range = (0, nside * nside), weights = tod[j, 2, 9, :]) 
+        nhit, edges   = np.histogram(px_idx[j, :], bins = nside * nside, range = (0, nside * nside))
         map /= nhit 
         map = np.nan_to_num(map, nan = 0)
-        histo += map.reshape(120, 120)     
+        histo += map.reshape(nside, nside)     
     infile.close()
 
 histo = np.where(histo != 0, histo * 1e6, np.nan) # Transforming K to muK
@@ -69,18 +71,21 @@ img = ax.imshow(histo.T / looplen, extent=(x_lim[0],x_lim[1],y_lim[0],y_lim[1]),
                     aspect=aspect, cmap=cmap, origin='lower',
                     vmin = -10000, vmax=10000)
 
+ax.axhline(y[60], color = "g", alpha = 0.5, zorder = 2)
+ax.axvline(x[60], color = "g", alpha = 0.5, zorder = 3)
+
 #ax.set_title(title)
 cbar = fig.colorbar(img)
 cbar.set_label("$\mu K$")
-plt.savefig("co6_l2_map.png")
-"""
+plt.savefig("co6_l2_cross_map.png")
 
+"""
+# L1
 data_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/level1/2020-07/"
-data_name = data_path + "comap-0015330-2020-07-31-040632_sim.hd5"
+data_name = data_path + "comap-0015330-2020-07-31-040632_sim_norm.hd5"
 
 nside       = 120
 histo = np.zeros((nside, nside))
-
 dpix        = 2.0 / 60.0
 fieldcent   = [226, 55]
 nfeeds      = 18
@@ -88,21 +93,22 @@ nbin        = nside * nside
 print("Loading TOD: ", time.time() - t0, " sec"); t0 = time.time()
 infile      = h5py.File(data_name, "r")
 tod         = np.array(infile["spectrometer/tod"]) 
-ra          =  np.array(infile["spectrometer/pixel_pointing/pixel_ra"])
-dec         =  np.array(infile["spectrometer/pixel_pointing/pixel_dec"])
+ra          = np.array(infile["spectrometer/pixel_pointing/pixel_ra"])
+dec         = np.array(infile["spectrometer/pixel_pointing/pixel_dec"])
 px_idx      = np.zeros_like(dec, dtype = int)
 tod[:, 0, :, :] = tod[:, 0, ::-1, :]
 tod[:, 2, :, :] = tod[:, 2, ::-1, :]
 looplen = 0
 print("Looping through feeds: ", time.time() - t0, " sec")
-for j in trange(nfeeds):  
+for j in trange(1):  
     looplen += 1
     px_idx[j, :] = WCS.ang2pix([nside, nside], [-dpix, dpix], fieldcent, dec[j, :], ra[j, :])
-    map, edges   = np.histogram(px_idx[j, :], bins = 120 * 120, range = (0, 120 * 120), weights = tod[j, 2, 9, :]) 
-    nhit, edges   = np.histogram(px_idx[j, :], bins = 120 * 120, range = (0, 120 * 120))
+    map, edges   = np.histogram(px_idx[j, :], bins = nside * nside, range = (0, nside * nside), weights = tod[j, 2, 9, :]) 
+    nhit, edges   = np.histogram(px_idx[j, :], bins = nside * nside, range = (0, nside * nside))
     map /= nhit 
+    print(edges)
     map = np.nan_to_num(map, nan = 0)
-    histo += nhit.reshape(120, 120)     
+    histo += map.reshape(nside, nside)     
 
 histo = np.where(histo != 0, histo * 1e6, np.nan) # Transforming K to muK
 
@@ -112,12 +118,31 @@ mapfile = h5py.File(mapfile_name, "r")
 x = np.array(mapfile["x"])
 y = np.array(mapfile["y"])
 
+X, Y = np.meshgrid(x, y, indexing = "ij")
+X, Y = X.flatten(), Y.flatten()
+pixvec = WCS.ang2pix([nside, nside], [-dpix, dpix], fieldcent, Y, X)
+_px_idx = np.unique(px_idx[0, :])
+
+for i in range(len(_px_idx)):
+    print(_px_idx[i], pixvec[_px_idx[i]-1])
+print(_px_idx)
+print(pixvec)
+print(np.max(pixvec), np.min(pixvec))
+
+fig2, ax2 = plt.subplots()
+ax2.plot(np.linspace(np.min(_px_idx), np.max(_px_idx), len(_px_idx)), _px_idx, label = "Pointing px")
+ax2.plot(np.linspace(np.min(pixvec[_px_idx]), np.max(pixvec[_px_idx]), len(pixvec[_px_idx])), pixvec[_px_idx], label = "Grid px")
+ax2.plot(np.linspace(np.min(pixvec[_px_idx - 1]), np.max(pixvec[_px_idx - 1]), len(pixvec[_px_idx - 1])), pixvec[_px_idx - 1], label = "Grid px - 1")
+ax2.set_yscale("symlog")
+ax2.legend()
+#ax2.plot(np.arange(np.min(pixvec[_px_idx-1]), np.max(pixvec[_px_idx-1])), pixvec[_px_idx])
+plt.savefig("test.png")
 x_lim, y_lim = [None,None], [None,None]
 dx = x[1] - x[0]
 x_lim[0] = x[0] - 0.5*dx; x_lim[1] = x[-1] + 0.5*dx
 dy = y[1] - y[0]
 y_lim[0] = y[1] - 0.5*dy; y_lim[1] = y[-1] + 0.5*dy
-
+print("lims: ", (x_lim[1] - x_lim[0]) / 120, (y_lim[1] - y_lim[0]) / 120)
 fig1, ax1 = plt.subplots(figsize=(10,6))
 
 matplotlib.use("Agg")  # No idea what this is. It resolves an error when writing gif/mp4.
@@ -136,5 +161,41 @@ img1 = ax1.imshow(histo.T / looplen, extent=(x_lim[0],x_lim[1],y_lim[0],y_lim[1]
 cbar = fig1.colorbar(img1)
 cbar.set_label("$\mu K$")
 plt.savefig("co6_l1_map.png")
+"""
+# Map
+"""
+mapfile_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/maps/sim/wo_mask/"
+mapfile_name = mapfile_path + "co6_map.h5"
+mapfile = h5py.File(mapfile_name, "r")
+x = np.array(mapfile["x"])
+y = np.array(mapfile["y"])
+map_coadd = np.array(mapfile["map_coadd"])
+map_coadd = map_coadd[2, 9, :, :]
+
+x_lim, y_lim = [None,None], [None,None]
+dx = x[1] - x[0]
+x_lim[0] = x[0] - 0.5*dx; x_lim[1] = x[-1] + 0.5*dx
+dy = y[1] - y[0]
+y_lim[0] = y[1] - 0.5*dy; y_lim[1] = y[-1] + 0.5*dy
+
+fig3, ax3 = plt.subplots(figsize=(10,6))
+
+matplotlib.use("Agg")  # No idea what this is. It resolves an error when writing gif/mp4.
+cmap_name = "CMRmap"
+cmap = copy.copy(plt.get_cmap(cmap_name))
+
+ax3.set_ylabel('Declination [deg]')
+ax3.set_xlabel('Right Ascension [deg]')
+
+aspect = dx/dy
+
+img = ax3.imshow(map_coadd * 1e6, extent=(x_lim[0],x_lim[1],y_lim[0],y_lim[1]), interpolation='nearest',
+                    aspect=aspect, cmap=cmap, origin='lower',
+                    vmin = -1e3, vmax=1e3, zorder = 1)
+
+ax3.axhline(y[60], color = "g", alpha = 0.5, zorder = 2)
+ax3.axvline(x[60], color = "g", alpha = 0.5, zorder = 3)
 
 
+plt.savefig("co6_cross_overlay_map.png")
+"""
--- src/sim/sim2TOD_v2.py
+++ src/sim/sim2TOD_v2.py
@@ -10,31 +10,46 @@ import argparse
 import re
 
 class Sim2TOD:
-    def __init__(self, cube_filename, tod_in_filename, tod_out_filename):
-        self.cube_filename = cube_filename  # Filepath of simulated cube.
-        self.tod_in_filename = tod_in_filename  # Filepath of already existing level1 file.
-        self.tod_out_filename = tod_out_filename  # Filepath of simulated output level1 file.
+    def __init__(self):#, cube_filename, tod_in_filename, tod_out_filename):
+        #self.cube_filename = cube_filename  # Filepath of simulated cube.
+        #self.tod_in_filename = tod_in_filename  # Filepath of already existing level1 file.
+        #self.tod_out_filename = tod_out_filename  # Filepath of simulated output level1 file.
         self.nside = 120   # Number of pixels in each direction.
         self.dpix = 2.0/60.0  # Pixel resolution in degrees (here, 2 arcminutes)
-        self.fieldcent = [226, 55]  # Center position of pixel-image, in degrees of ra/dec. CO6
+        #self.fieldcent = [226, 55]  # Center position of pixel-image, in degrees of ra/dec. CO6
         #self.fieldcent = [170, 52.5]  # Center position of pixel-image, in degrees of ra/dec. CO7
+        self.input()
 
     def run(self):
-        print("Loading Cube"); t0 = time.time()
+        print("Processing Parameterfile: "); t0 = time.time()
+        self.read_paramfile()
+        
+        print("Time: ", time.time()-t0, " sec")
+        print("Loading Cube: "); t0 = time.time()
         self.load_cube()
+        
         print("Time: ", time.time()-t0, " sec")
-        print("Copying outfile"); t0 = time.time()
-        self.make_outfile()
-        print("Time: ", time.time()-t0, " sec")
-        print("Loading TOD"); t0 = time.time()        
-        self.load_tod()
-        print("Time: ", time.time()-t0, " sec")
-        print("Calculating Tsys"); t0 = time.time()        
-        self.calc_tsys()
-        print("Time: ", time.time()-t0, " sec")
-        print("Writing sim-data to TOD"); t0 = time.time()
-        self.write_sim()
-        print("Time: ", time.time()-t0, " sec")
+        print("Copying Outfile: "); t0 = time.time()
+        
+        for i in range(len(self.tod_in_list)):
+            self.tod_in_filename    = self.tod_in_path + self.tod_in_list[i]
+            self.tod_out_filename   = self.tod_out_path + self.tod_in_list[i]
+            
+            self.make_outfile()
+        
+            print("Time: ", time.time()-t0, " sec")
+            print("Loading TOD: "); t0 = time.time()        
+            self.load_tod()
+        
+            print("Time: ", time.time()-t0, " sec")
+            print("Calculating Tsys: "); t0 = time.time()        
+            self.calc_tsys()
+        
+            print("Time: ", time.time()-t0, " sec")
+            print("Writing sim-data to TOD: "); t0 = time.time()
+            self.write_sim()
+        
+            print("Time: ", time.time()-t0, " sec")
 
     def input(self):
         parser = argparse.ArgumentParser()
@@ -48,49 +63,53 @@ class Sim2TOD:
     def read_paramfile(self):
         param_file = open(self.param_file, "r")
         params = param_file.read()
-        runlist_path = re.search(r"RUNLIST\s*=\s*'(\/.*?)'", params)
+
+        runlist_path = re.search(r"\nRUNLIST\s*=\s*'(\/.*?)'", params)
         self.runlist_path = str(runlist_path.group(1))
         
-        tod_in_path = re.search(r"LEVEL1_DIR\s*=\s*'(\/.*?)'", params)
+        tod_in_path = re.search(r"\nLEVEL1_DIR\s*=\s*'(\/.*?)'", params)
         self.tod_in_path = str(tod_in_path.group(1))
         
-        tod_out_path = re.search(r"SIM_LEVEL1_DIR\s*=\s*'(\/.*?)'", params)
+        tod_out_path = re.search(r"\nSIM_LEVEL1_DIR\s*=\s*'(\/.*?)'", params)
         self.tod_out_path = str(tod_out_path.group(1))
-
-        cube_path = re.search(r"DATACUBE\s*=\s*'(\/.*?\.\w+)'", params)
-        self.cube_path = str(cube_path.group(1))
+        
+        cube_path = re.search(r"\nDATACUBE\s*=\s*'(\/.*?\.\w+)'", params)
+        self.cube_filename = str(cube_path.group(1))
 
         runlist_file = open(self.runlist_path, "r")
         runlist = runlist_file.read()
-        l1_file_list = re.findall(r"\/.*?\.\w+", runlist)
-        self.l1_file_list = [self.tod_in_path + i for i in l1_file_list]
+        tod_in_list = re.findall(r"\/.*?\.\w+", runlist)
+        self.tod_in_list = tod_in_list
+
+        patch_name = re.search(r"\s([a-zA-Z0-9]+)\s", runlist)
+        self.patch_name = str(patch_name.group(1))
+
+        patch_def_path = re.search(r"\nPATCH_DEFINITION_FILE\s*=\s*'(\/.*?)'", params)
+        self.patch_def_path = str(patch_def_path.group(1))
+
+        patch_def_file = open(self.patch_def_path, "r")
+        patch_def = patch_def_file.read()
+        fieldcent   = re.search(rf"{self.patch_name}\s*([0-9.]+)\s*([0-9.]+)", patch_def) 
+        self.fieldcent = [eval(fieldcent.group(1)), eval(fieldcent.group(2))]
+
+        print("Patch def:", self.patch_def_path)
+        print("Patch", self.patch_name)
+        print("Field center", self.fieldcent)
         print("Runlist:", self.runlist_path)
         print("TOD in:", self.tod_in_path)
         print("TOD out:", self.tod_out_path)
-        print("Cube:", self.cube_path)
-        print("# obsID", len(self.l1_file_list))
-        print("obsID #1: ", self.l1_file_list[0])
+        print("Cube:", self.cube_filename)
+        print("# obsID", len(self.tod_in_list))
+        print("obsID #1: ", self.tod_in_list[0])
         #sys.exit()
-        """
-        for line in param_file:
-            params = re.split("= |' | ", line)
-            if "RUNLIST" in params:
-                self.runlist = params[-2]
-                print("params: ", params)
-                print(self.runlist)
-        """
     
     def load_cube(self):
         """
         Read the simulated datacube into memory.
         """
-        cube        = np.load(cube_filename)
-        cubeshape   = cube.shape
-        #cube       /= np.max(cube)
-        maxval         = np.max(cube)
-        cube        = np.zeros(cubeshape)
-        cube[:, 60, :] = 10 * maxval
-        cube[60, :, :] = 10 * maxval
+        cube = np.load(cube_filename)
+        cubeshape = cube.shape
+        cube /= np.max(cube)
         cube = cube.reshape(cubeshape[0]*cubeshape[1], 4, 1024)  # Flatten the x/y dims, and split the frequency (depth) dim in 4 sidebands.
         cube = cube.transpose(1, 2, 0)  # Reorder dims such that the x/y dim is last, and the frequencies first (easier to deal with later).
         cube[0, :, :] = cube[0, ::-1, :]
@@ -110,18 +129,19 @@ class Sim2TOD:
         infile        = h5py.File(self.tod_in_filename, "r")
 
         self.tod      = np.array(infile["/spectrometer/tod"])
-        self.freqs    = np.array(infile["/spectrometer/frequency"])
-        self.tod_time = np.array(infile["/spectrometer/MJD"])
+        #self.freqs    = np.array(infile["/spectrometer/frequency"])
+        #self.tod_time = np.array(infile["/spectrometer/MJD"])
         self.feeds    = np.array(infile["/spectrometer/feeds"])
         self.nfeeds   = len(self.feeds)
         self.ra       = np.array(infile["/spectrometer/pixel_pointing/pixel_ra"])
         self.dec      = np.array(infile["/spectrometer/pixel_pointing/pixel_dec"])
         self.tod_sim  = self.tod.copy()  # The simulated data is, initially, simply a copy of the original TOD.
-        self.vane_angles    = np.array(infile["/hk/antenna0/vane/angle"])/100.0  # Degrees
-        self.vane_time      = np.array(infile["/hk/antenna0/vane/utc"])
-        self.array_features = np.array(infile["/hk/array/frame/features"])
+        #self.vane_angles    = np.array(infile["/hk/antenna0/vane/angle"])/100.0  # Degrees
+        #self.vane_time      = np.array(infile["/hk/antenna0/vane/utc"])
+        #self.array_features = np.array(infile["/hk/array/frame/features"])
         self.infile = infile
-        
+        infile.close()
+
     def calc_tsys(self):
         self.tsys = 55.0  # Hard-coded Tsys value. Needs to be calculated.
 
@@ -133,13 +153,14 @@ class Sim2TOD:
             pixvec[i, :] = WCS.ang2pix([nside, nside], [-dpix, dpix], fieldcent, dec[i, :], ra[i, :])     
             # Update tod_sim values.
             #self.tod_sim[i, :, :, :] += np.nanmean(np.array(tod[i, :, :, :]), axis=2)[ :, :, None] * cube[ :, :, pixvec[i, :]] / tsys
+            t = time.time()
             self.tod_sim[i, :, :, :] *= 1 + cube[ :, :, pixvec[i, :]] / tsys
             #self.tod_sim[i, :, :, :] *= 1 + cube[ :, :, pixvec[i, :]] * np.nanmax(tod)
-        
+            print("Adding to tod: ", time.time() - t, " sec")
         with h5py.File(self.tod_out_filename, "r+") as outfile:  # Write new sim-data to file.
             data = outfile["/spectrometer/tod"] 
             data[...] = self.tod_sim
-            
+        outfile.close()
 if __name__ == "__main__":
     cube_path = "/mn/stornext/d16/cmbco/comap/protodir/"
     cube_filename = cube_path + "cube_real.npy"
@@ -152,8 +173,8 @@ if __name__ == "__main__":
     tod_out_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/level1/2020-07/"
     tod_out_filename = tod_out_path + "comap-0015330-2020-07-31-040632_sim_cross.hd5"
 
-    sim2tod = Sim2TOD(cube_filename, tod_in_filename, tod_out_filename)
+    sim2tod = Sim2TOD()#cube_filename, tod_in_filename, tod_out_filename)
     #sim2tod.input()
     #sim2tod.read_paramfile()
-    #sys.exit()
     sim2tod.run()
+    sys.exit()
