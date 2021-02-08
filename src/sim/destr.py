4f83f4cf25dc1120469db948e7bad94949d4a106
SagittariusA <nilsole2009@live.no>
2021-02-08 11:33:23 +0100

    Changed tod buffer dtype to float32 for better memory handling

--- src/sim/destr.py
+++ src/sim/destr.py
@@ -14,6 +14,7 @@ import WCS
 import copy
 import os
 import re
+import argparse
 
 class Destriper():
     def __init__(self, eps = 0, param_file = None, infile_path = None, outfile_path = None, obsID_map = False):
@@ -31,9 +32,10 @@ class Destriper():
             self.read_paramfile()
         else:
             self.input()
+            self.read_paramfile()
         
         if self.scheme not in ["destriper", "weighted", "avg", "baseline_only"]:
-            print("Please provide one of the allowed mapmaker schemes: 'destriper', 'weithed' or 'avg'")
+            print("Please provide one of the allowed mapmaker schemes: 'destriper', 'weighted', 'avg' or 'baseline_only'.")
             sys.exit()
         
         
@@ -123,7 +125,16 @@ class Destriper():
         masking = re.search(r"\nUSE_MASK\s*=\s*([01])", params)  # Regex pattern to search for patch definition file.
         self.masking = bool(int(masking.group(1)))
         
-        Nproc = re.search(r"\nN_FREQ_PROCESS\s*=\s*(\d{3})", params)  # Regex pattern to search for number of frequency processes to run.
+        baseline_only = re.search(r"\nFIT_BASELINES\s*=\s*(.true.|.false.)", params)  # Regex pattern to search for patch definition file.
+        self.baseline_only = baseline_only.group(1)
+        if self.baseline_only == ".true.":
+            self.baseline_only = True 
+            self.scheme = "baseline_only"
+        else:
+            self.baseline_only = False 
+
+        Nproc = re.search(r"\nN_FREQ_PROCESS\s*=\s*(\d+)", params)  # Regex pattern to search for number of frequency processes to run.
+
         self.Nproc = int(Nproc.group(1))
         
         runlist_file.close()    
@@ -141,6 +152,8 @@ class Destriper():
         print("Number of obs:", self.nobsIDs)
         print("Map output name:", self.map_name)
         print("Mapmaker scheme:", self.scheme)
+        print("Fit baseline only:", self.baseline_only)
+
         print("Baseline freq:", self.basefreq)
         print("Highpass cut:", self.highpass_nu)
         print("Use mask:", self.masking)
@@ -187,7 +200,7 @@ class Destriper():
         if self.obsID_map:
             print("obsID ", self.obsID)
             for filename in os.listdir(self.infile_path):
-                if self.obsID in filename:
+                if self.obsID in filename and len(filename) <= 17:
                     if np.any([(name in filename) for name in self.scanIDs]):
                         Nscans += 1
                         infile = h5py.File(self.infile_path + filename, "r")
@@ -223,9 +236,6 @@ class Destriper():
         freq[0, :] = freq[0, ::-1]
         freq[2, :] = freq[2, ::-1]   
         self.freq  = freq
-        self.tod_first = np.array(infile["tod"])[:-1,...]
-        self.tod_first[:, 0, :, :] = self.tod_first[:, 0, ::-1, :]
-        self.tod_first[:, 2, :, :] = self.tod_first[:, 2, ::-1, :]
         infile.close()
         
         print("Number of scans:", Nscans)
@@ -236,12 +246,13 @@ class Destriper():
         tod_cumlen[1:] = np.cumsum(tod_lens).astype(int)
         Nsamp_tot = np.sum(tod_lens)
     
-        self.tod_buffer = np.zeros((Nsamp_tot, Nsb, Nfreq))
+        self.tod_buffer = np.zeros((Nsamp_tot, Nsb, Nfreq), dtype = np.float32)
+       
         #time_buffer = np.zeros(Nsamp_tot)
-        ra_buffer = np.zeros(Nsamp_tot)
-        dec_buffer = np.zeros(Nsamp_tot)
-        self.sigma0_buffer = np.zeros((Nscans, Nfeed, Nsb, Nfreq))
-        self.mask_buffer = np.zeros((Nscans, Nfeed, Nsb, Nfreq))
+        ra_buffer = np.zeros(Nsamp_tot, dtype = np.float32)
+        dec_buffer = np.zeros(Nsamp_tot, dtype = np.float32)
+        self.sigma0_buffer = np.zeros((Nscans, Nfeed, Nsb, Nfreq), dtype = np.float32)
+        self.mask_buffer = np.zeros((Nscans, Nfeed, Nsb, Nfreq), dtype = np.uint8)
         
         Nbaseline_tot = 0
         Nperbaselines = [0]
@@ -257,8 +268,6 @@ class Destriper():
             freqmask[:, 0, :] = freqmask[:, 0, ::-1] #[()]#.astype(dtype=np.float32, copy=False) 
             freqmask[:, 2, :] = freqmask[:, 2, ::-1] #[()]#.astype(dtype=np.float32, copy=False) 
             freqmask          = freqmask[:-1, :, :]
-            
-            
             
             tod                = np.array(infile["tod"])[()] #[()]#.astype(dtype=np.float32, copy=False) 
             
@@ -414,7 +423,6 @@ class Destriper():
         self.P = csc_matrix((hits, (rows, cols)), shape = (Nsamp, Npix))
         self.PT = csc_matrix(self.P.T)
         
-        
     def get_F(self):
         Nsamp, Nbaseline, Nperbaselines = self.Nsamp, self.Nbaseline, self.Nperbaselines
         Nperbaselines_cum = np.zeros(Nbaseline + 1)
@@ -443,39 +451,37 @@ class Destriper():
         self.C_n_inv = diags(C_n_inv)
     
     def get_PCP_inv(self):
-        P, PT, C_n_inv = self.P, self.PT, self.C_n_inv
+        #P, PT, C_n_inv, eps = self.P, self.PT, self.C_n_inv, self.eps
         
-        PCP_inv = PT.dot(C_n_inv)        
-        PCP_inv = PCP_inv.dot(P) + diags(eps * np.ones(self.Npix))
+        PCP_inv = self.PT.dot(self.C_n_inv)        
+        PCP_inv = PCP_inv.dot(self.P) + diags(self.eps * np.ones(self.Npix))
            
         self.PCP_inv = diags(1 / PCP_inv.diagonal(), format = "csc")
    
     def get_FT_C_P_PCP(self):
-        FT, C_n_inv, P, PCP_inv = self.FT, self.C_n_inv, self.P, self.PCP_inv    
+        #FT, C_n_inv, P, PCP_inv = self.FT, self.C_n_inv, self.P, self.PCP_inv    
         
-        FT_C = FT.dot(C_n_inv)        
-        FT_C_P = FT_C.dot(P)
+        FT_C = self.FT.dot(self.C_n_inv)        
+        FT_C_P = FT_C.dot(self.P)
         
-        self.FT_C_P_PCP = FT_C_P.dot(PCP_inv)
+        self.FT_C_P_PCP = FT_C_P.dot(self.PCP_inv)
     
     def get_PT_C(self):
-        PT, C_n_inv = self.PT, self.C_n_inv
+        #PT, C_n_inv = self.PT, self.C_n_inv
         
-        self.PT_C = PT.dot(C_n_inv)
+        self.PT_C = self.PT.dot(self.C_n_inv)
         
-    
     def get_FT_C(self):
-        FT, C_n_inv = self.FT, self.C_n_inv
+        #FT, C_n_inv = self.FT, self.C_n_inv
 
-        self.FT_C = FT.dot(C_n_inv)        
+        self.FT_C = self.FT.dot(self.C_n_inv)        
     
-        
     def Ax(self, a):
-        FT_C_F, FT_C_P_PCP, PT_C_F = self.FT_C_F, self.FT_C_P_PCP, self.PT_C_F
+        #FT_C_F, FT_C_P_PCP, PT_C_F = self.FT_C_F, self.FT_C_P_PCP, self.PT_C_F
       
-        temp0 = FT_C_F.dot(a)
-        temp1 = PT_C_F.dot(a)
-        temp2 = FT_C_P_PCP.dot(temp1)
+        temp0 = self.FT_C_F.dot(a)
+        temp1 = self.PT_C_F.dot(a)
+        temp2 = self.FT_C_P_PCP.dot(temp1)
         
         #print(a)
         #print("CG counter: ", self.counter)
@@ -484,11 +490,11 @@ class Destriper():
         return temp0 - temp2
         
     def b(self, x):        
-        FT_C, FT_C_P_PCP, PT_C = self.FT_C, self.FT_C_P_PCP, self.PT_C
+        #FT_C, FT_C_P_PCP, PT_C = self.FT_C, self.FT_C_P_PCP, self.PT_C
 
-        temp0 = FT_C.dot(x)
-        temp1 = PT_C.dot(x)
-        temp2 = FT_C_P_PCP.dot(temp1)
+        temp0 = self.FT_C.dot(x)
+        temp1 = self.PT_C.dot(x)
+        temp2 = self.FT_C_P_PCP.dot(temp1)
 
         return temp0 - temp2
 
@@ -512,15 +518,15 @@ class Destriper():
 
     
     def get_destriped_map(self):
-        P, PT, C_n_inv, F, tod, Nsamp, eps, PCP_inv = self.P, self.PT, self.C_n_inv, self.F, self.tod, self.Nsamp, self.eps, self.PCP_inv
+        #P, PT, C_n_inv, F, tod, Nsamp, eps, PCP_inv = self.P, self.PT, self.C_n_inv, self.F, self.tod, self.Nsamp, self.eps, self.PCP_inv
 
         self.get_baselines()
 
-        m = PCP_inv.dot(PT).dot(C_n_inv).dot(tod - F.dot(self.a))
+        m = self.PCP_inv.dot(self.PT).dot(self.C_n_inv).dot(self.tod - self.F.dot(self.a))
         self.m = m.reshape(self.Nside, self.Nside)
 
     def get_bin_averaged_map(self):
-        P, PT, tod, eps = self.P, self.PT, self.tod, self.eps
+        #P, PT, tod, eps = self.P, self.PT, self.tod, self.eps
         
         cut = self.highpass_nu
         if self.highpass_nu > 0:
@@ -529,16 +535,16 @@ class Destriper():
             highpass = False
             
         if highpass:
-            tod = np.fft.rfft(tod)
+            self.tod = np.fft.rfft(self.tod)
             freqs   = np.fft.rfftfreq(self.Nsamp, self.dt)
             cut_idx = np.argmin(np.abs(freqs - cut))
-            tod[0:cut_idx] = 0
-            tod     = np.fft.irfft(tod[1:])
+            self.tod[0:cut_idx] = 0
+            self.tod     = np.fft.irfft(self.tod[1:])
             #sos = signal.butter(10, cut, "highpass", output = "sos")
             #tod = signal.sosfilt(sos, tod)
             
-        A = PT.dot(P) + diags(eps * np.ones(self.Npix))
-        b = PT.dot(tod)
+        A = self.PT.dot(self.P) + diags(self.eps * np.ones(self.Npix))
+        b = self.PT.dot(self.tod)
         
         #A = sparse_dot_mkl.dot_product_mkl(PT, P) + diags(eps * np.ones(self.Npix))
         #b = sparse_dot_mkl.dot_product_mkl(PT, tod)
@@ -554,15 +560,15 @@ class Destriper():
         else:
             highpass = False
         
-        P, PT, C_n_inv, tod, Nsamp, eps, PCP_inv = self.P, self.PT, self.C_n_inv, self.tod, self.Nsamp, self.eps, self.PCP_inv 
+        #P, PT, C_n_inv, tod, Nsamp, eps, PCP_inv = self.P, self.PT, self.C_n_inv, self.tod, self.Nsamp, self.eps, self.PCP_inv 
         
         if highpass:
             print("Highpass filtering TOD")
-            tod = np.fft.rfft(tod)
-            freqs   = np.fft.rfftfreq(Nsamp, self.dt)
+            self.tod = np.fft.rfft(self.tod)
+            freqs   = np.fft.rfftfreq(self.Nsamp, self.dt)
             cut_idx = np.argmin(np.abs(freqs - cut))
-            tod[0:cut_idx] = 0
-            tod     = np.fft.irfft(tod, n = Nsamp)
+            self.tod[0:cut_idx] = 0
+            self.tod     = np.fft.irfft(self.tod, n = self.Nsamp)
             #sos = signal.butter(10, cut, "highpass", output = "sos")
             #tod = signal.sosfilt(sos, tod)
             
@@ -570,7 +576,7 @@ class Destriper():
         #PCP_inv = PCP_inv.dot(P) + diags(eps * np.ones(self.Npix))
         #PCP_inv = diags(1 / PCP_inv.diagonal())
         
-        self.m = PCP_inv.dot(PT).dot(C_n_inv).dot(tod).reshape(self.Nside, self.Nside)
+        self.m = self.PCP_inv.dot(self.PT).dot(self.C_n_inv).dot(self.tod).reshape(self.Nside, self.Nside)
     
     def make_map(self):
         if self.scheme == "destriper":
@@ -582,24 +588,24 @@ class Destriper():
     
     def make_baseline_only(self):
         self.get_baselines()
-        #self.fill_baseline_tod_buffer()
         self.baseline_tod = self.F.dot(self.a) 
+        #self.baseline_tod = self.tod.copy() 
 
     def get_hits(self):
         #self.get_P()
-        P, PT, Nside, Nsamp = self.P, self.PT, self.Nside, self.Nsamp
+        #P, PT, Nside, Nsamp = self.P, self.PT, self.Nside, self.Nsamp
         #ones = np.ones(Nsamp)
-        self.hits = PT.dot(P).diagonal().reshape(Nside, Nside) #PT.dot(ones).reshape(Nside, Nside)
+        self.hits = self.PT.dot(self.P).diagonal().reshape(self.Nside, self.Nside)
         
     def get_rms(self):
         #self.get_P()
         #self.get_Cn_inv()
-        P, C_n_inv, Nsamp, Nside, eps, PCP_inv = self.P, self.C_n_inv, self.Nsamp, self.Nside, self.eps, self.PCP_inv
+        #P, C_n_inv, Nsamp, Nside, eps, PCP_inv = self.P, self.C_n_inv, self.Nsamp, self.Nside, self.eps, self.PCP_inv
         
         #PCP_inv = P.transpose().dot(C_n_inv)        
         #PCP_inv = PCP_inv.dot(P) + diags(eps * np.ones(self.Npix))
         #PCP_inv = diags(1 / PCP_inv.diagonal())
-        self.rms = np.sqrt(PCP_inv.diagonal().reshape(Nside, Nside))
+        self.rms = np.sqrt(self.PCP_inv.diagonal().reshape(self.Nside, self.Nside))
         
     def load_cube(self):
         """
@@ -627,8 +633,6 @@ class Destriper():
         #self.outfile = self.map_out_path + f"{self.patch_name}_{self.out_name}"         
         #print(self.outfile)
         
-        print(Nside, dpix, fieldcent, dpix / np.cos(np.radians(fieldcent[1])))
-        print(full_map.shape, full_hits.shape, full_rms.shape, fieldcent.shape, freq.shape)
         x = np.zeros(Nside)
         y = np.zeros(Nside)
         dx = dpix / np.cos(np.radians(fieldcent[1]))
@@ -642,10 +646,8 @@ class Destriper():
             x_min = fieldcent[0] - dx * Nside / 2 - dx / 2
             y_min = fieldcent[1] - dy * Nside / 2  - dy / 2
             
-        print(x_min, y_min, fieldcent, dx * Nside / 2, dy * Nside / 2)
         x[0] = x_min + dx / 2
         y[0] = y_min + dy / 2
-        print(x[0], y[0])
         
         for i in range(1, Nside):
             x[i] = x[i - 1] + dx
@@ -668,20 +670,18 @@ class Destriper():
             
         outfile.close()
 
-    def fill_baseline_tod_buffer(self, baselines):
-        print("Filling baseline TOD buffer:")
-        #self.corrected_tod_buffer[:, self.freq_idx] = self.tod - self.F.dot(self.a)
-        self.baseline_buffer[:, self.freq_idx]      = self.F.dot(self.a)
-
     def save_baseline_tod(self, baseline_buffer):
         print("Saving baselines:")
         tod_lens = self.tod_lens
         tod_lens = tod_lens[::self.Nfeed]
 
-        outfile_path = self.infile_path + "baselines/"
+        #outfile_path = self.infile_path + "baselines/"
+        outfile_path = "/mn/stornext/d16/cmbco/comap/nils/COMAP_general/data/level2/Ka/sim/highpass/002Hz/default/XL_dataset/co6/baselines/"
+        #outfile_path = self.infile_path + "null_test/"
+        
+        print("Saveing baselines to:", outfile_path)
         if not os.path.exists(outfile_path):
             os.mkdir(outfile_path)
-            print(outfile_path)
         
         for i in range(len(self.names)):
             start, stop = self.start_stop[:, i]
@@ -692,12 +692,16 @@ class Destriper():
             baseline = baseline.reshape(shape[0], 4, 64)
             baseline = baseline.reshape(self.Nfeed, tod_lens[i], 4, 64)
             baseline = baseline.transpose(0, 2, 3, 1)
+            baseline[:, 0, :, :] = baseline[:, 0, ::-1, :]
+            baseline[:, 2, :, :] = baseline[:, 2, ::-1, :]
 
+            baseline = baseline.astype(np.float32)
+            
             new_name = self.names[i].split(".")
             new_name = new_name[0] + "_temp." + new_name[1]
             
             infile = h5py.File(outfile_path + new_name, "w")
-            infile.create_dataset("tod_baseline", data = baseline)
+            infile.create_dataset("tod_baseline", data = baseline, dtype = "float32")
             infile.close()
 
 if __name__ =="__main__":
@@ -757,7 +761,7 @@ if __name__ =="__main__":
     
     for pfile in paramfiles:
         t = time.time()
-        destr = Destriper(eps, param_file = pfile)
+        destr = Destriper(param_file = pfile)
         print("Loading data and initializing pointing:")
         t0 = time.time()
         destr.get_data()
@@ -772,23 +776,19 @@ if __name__ =="__main__":
             t = time.time()
             destr.run(freq_idx = idx)
 
-            if destr.scheme == "baseline_only":
-                destr.make_baseline_only()
-                return np.array([destr.baseline_tod])
-            else:
-                destr.make_map()
-                print("\n", "Making map: ", time.time() - t, "sec \n")
+            destr.make_map()
+            print("\n", "Making map: ", time.time() - t, "sec \n")
 
-                destr.get_rms()
-                print("\n", "Making rms map: ", time.time() - t, "sec \n")
+            destr.get_rms()
+            print("\n", "Making rms map: ", time.time() - t, "sec \n")
 
-                destr.get_hits()
-                print("\n", "Making hit map: ", time.time() - t, "sec \n")
+            destr.get_hits()
+            print("\n", "Making hit map: ", time.time() - t, "sec \n")
 
-                maps = np.array([destr.m, destr.rms, destr.hits])
-                maps = np.where(np.isnan(maps) == False, maps, 0)
-                return np.array([destr.m, destr.rms, destr.hits])
-        
+            maps = np.array([destr.m, destr.rms, destr.hits])
+            maps = np.where(np.isnan(maps) == False, maps, 0)
+            return np.array([destr.m, destr.rms, destr.hits])
+    
         with multiproc.Pool(processes = N_proc) as pool:
             full_map = pool.map(dummy, freq_idx)
         pool.close()
