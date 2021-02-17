a22fb212765f9f57a6402dbea151f9df3ade09f8
SagittariusA <nilsole2009@live.no>
2021-02-17 15:06:01 +0100

    Adding make_baseline.py

--- src/sim/make_baselines.py
+++ src/sim/make_baselines.py
@@ -14,44 +14,42 @@ import WCS
 import copy
 import os
 import re
+import argparse
 from destr import Destriper 
 
-    #freq_idx = [113]
-    freq_idx = range(4 * 64)
-    
+freq_idx = range(4 * 64)
+
+t = time.time()
+destr = Destriper()
+print("Loading data and initializing pointing:")
+t0 = time.time()
+destr.get_data()
+destr.initialize_P_and_F()
+print("Loading time:", time.time() - t0, "sec")
+
+t0 = time.time()
+print("Looping over frequencies:")
+
+def dummy(idx):
+    print("\n", "Processing frequency number:", idx, "\n")
     t = time.time()
-    destr = Destriper()
-    print("Loading data and initializing pointing:")
-    t0 = time.time()
-    destr.get_data()
-    destr.initialize_P_and_F()
-    print("Loading time:", time.time() - t0, "sec")
-    
-    t0 = time.time()
-    print("Looping over frequencies:")
+    destr.run(freq_idx = idx)
 
-    def dummy(idx):
-        print("\n", "Processing frequency number:", idx, "\n")
-        t = time.time()
-        destr.run(freq_idx = idx)
+    destr.make_baseline_only()
+    return np.array([destr.baseline_tod])
+    
+with multiproc.Pool(processes = destr.Nproc) as pool:
+    baselines = pool.map(dummy, freq_idx)
+pool.close()
+pool.join()
+print("Finished frequency loop:", time.time() - t0, "sec")
 
-        destr.make_baseline_only()
-        return np.array([destr.baseline_tod])
-        
+baselines = np.array(baselines)
+baselines = baselines[:, 0, :]
+baselines = baselines.transpose()
+
+#destr.save_baseline_tod(baselines)
+print("Time: ", time.time() - t0, "sec")
     
-    with multiproc.Pool(processes = destr.Nproc) as pool:
-        full_map = pool.map(dummy, freq_idx)
-    pool.close()
-    pool.join()
-    print("Finished frequency loop:", time.time() - t0, "sec")
     
-    print("Find baselines only:")
-    baselines = np.array(full_map)
-    baselines = baselines[:, 0, :]
-    baselines = baselines.transpose()
-    
-    destr.save_baseline_tod(baselines)
-    print("Baselines only:", time.time() - t0, "sec")
-        
-        
 
